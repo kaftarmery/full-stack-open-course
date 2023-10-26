ca5b895d58d864d783c9b4fcc75bedf111dacd6c
meryemkaftar <merykft@gmail.com>
2023-10-26 16:12:26 +0300

    part1 - exercise 1.2

--- part1/courseone/src/App.jsx
+++ part1/courseone/src/App.jsx
@@ -1,23 +1,14 @@
 const App = () => {
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
+
   const Header = (props) => {
     return <h1>{props.course}</h1>;
-  };
-
-  const Content = (props) => {
-    return (
-      <>
-        <p>
-          {props.part1} {props.exercises1}
-        </p>
-        <p>
-          {props.part2} {props.exercises2}
-        </p>
-
-        <p>
-          {props.part3} {props.exercises3}
-        </p>
-      </>
-    );
   };
 
   const Total = (props) => {
@@ -28,27 +19,33 @@ const App = () => {
     );
   };
 
-  const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
+  const Part = (props) => {
+    return (
+      <>
+        <p>
+          {/* instead of writing "props.part1/part2/part3 and props.exercises1/exercises2. We simply named the two "props" as part and exercise.
+          Later in the Content component, we'll ASSIGN whatever value we want for the props to render. */}
+          {props.part} {props.exercises}
+        </p>
+      </>
+    );
+  };
 
+  const Content = () => {
+    return (
+      <>
+        <Part part={part1} exercises={exercises1} />
+        <Part part={part2} exercises={exercises2} />
+        <Part part={part3} exercises={exercises3} />
+      </>
+    );
+  };
+
+  // what is rendered on the screen. this is the "return" of the App.jsx
   return (
     <div>
       <Header course={course} />
-
-      <Content
-        part1={part1}
-        exercises1={exercises1}
-        part2={part2}
-        exercises2={exercises2}
-        part3={part3}
-        exercises3={exercises3}
-      />
-
+      <Content />
       <Total totalExercises={exercises1 + exercises2 + exercises3} />
     </div>
   );
