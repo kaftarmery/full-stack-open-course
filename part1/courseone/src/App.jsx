599ca9291e8a5fadf90fa577563cf7fbd0845418
meryemkaftar <merykft@gmail.com>
2023-10-30 12:58:36 +0300

    part1 - exercise 1.5

--- part1/courseone/src/App.jsx
+++ part1/courseone/src/App.jsx
@@ -1,44 +1,32 @@
 const App = () => {
-  const course = "Half Stack application development";
-  const parts = [
-    {
-      name: "Fundamentals of React",
-      exercises: 10,
-    },
-    {
-      name: "Using props to pass data",
-      exercises: 7,
-    },
-    {
-      name: "State of a component",
-      exercises: 14,
-    },
-  ];
-
-  const Header = (props) => {
-    return <h1>{props.course}</h1>;
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
   };
 
-  const Total = (props) => {
-    // USED REDUCE TO GET THE FUM OF THE ARRAY
-    return (
-      <>
-        <p>
-          Total of exercises:{" "}
-          {props.parts.reduce((a, e) => {
-            return a + e.exercises;
-          }, 0)}
-        </p>
-      </>
-    );
+  const Header = (props) => {
+    return <h1>{props.name}</h1>;
   };
 
   const Part = (props) => {
+    const { partName, theExercise } = props;
     return (
       <>
         <p>
-          {/* {props.part} {props.exercises} */}
-          {props.parts.name} {props.parts.exercises}
+          {partName} - {theExercise}
         </p>
       </>
     );
@@ -46,24 +34,32 @@ const App = () => {
 
   const Content = (props) => {
     return (
-      <>
-        {/* <Part part={parts[0].name} exercises={parts[0].exercises} />
-        <Part part={parts[1].name} exercises={parts[1].exercises} />
-        <Part part={parts[2].name} exercises={parts[2].exercises} /> */}
+      <div>
+        {props.allParts.map((part, index) => (
+          <Part key={index} partName={part.name} theExercise={part.exercises} />
+        ))}
+      </div>
+    );
+  };
 
-        <Part parts={props.parts[0]} />
-        <Part parts={props.parts[1]} />
-        <Part parts={props.parts[2]} />
+  const Total = (props) => {
+    return (
+      <>
+        <p>
+          Total of exercises:{" "}
+          {props.total.reduce((a, e) => {
+            return a + e.exercises;
+          }, 0)}
+        </p>
       </>
     );
   };
 
-  // what is rendered on the screen. this is the "return" of the App.jsx
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header name={course.name} />
+      <Content allParts={course.parts} />
+      <Total total={course.parts} />
     </div>
   );
 };
